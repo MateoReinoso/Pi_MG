27800ca867074ee1274ec4b342d1768ccaf3c678
MateoReinoso <dmreinoso2@espe.edu.ec>
2020-07-02 19:47:29 -0500

    validacion firebase

--- Desarrollo_MG/app/src/main/java/com/pm/pi_mg/LoginActivity.java
+++ Desarrollo_MG/app/src/main/java/com/pm/pi_mg/LoginActivity.java
@@ -1,14 +1,69 @@
 package com.pm.pi_mg;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextInputEditText mTextInputEmail;
+    TextInputEditText mTextInputPassword;
+    Button mButtonLogin;
+
+
+    FirebaseAuth mAuth;
+    DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mTextInputEmail = findViewById(R.id.textInputEmail);
+        mTextInputPassword = findViewById(R.id.textInputPassword);
+
+        mButtonLogin = findViewById(R.id.btnLogin);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+    }
+
+    private void login() {
+        String email = mTextInputEmail.getText().toString();
+        String password = mTextInputPassword.getText().toString();
+
+        if(!email.isEmpty() && !password.isEmpty()){
+            if(password.length() >= 6){
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this, "Ingreso correcto", Toast.LENGTH_SHORT).show();
+                        }else {
+                            Toast.makeText(LoginActivity.this, "El email o contraseña son incorrectos.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }
     }
 }
--- Desarrollo_MG/app/src/main/java/com/pm/pi_mg/SelectOptionAuthActivity.java
+++ Desarrollo_MG/app/src/main/java/com/pm/pi_mg/SelectOptionAuthActivity.java
@@ -1,14 +1,39 @@
 package com.pm.pi_mg;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SelectOptionAuthActivity extends AppCompatActivity {
+
+    Toolbar mToolbar;
+    Button mButtonGoToLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_option_auth);
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Seleccionar opcion");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mButtonGoToLogin = findViewById(R.id.btnGoToLogin);
+        mButtonGoToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToLogin();
+            }
+        });
+    }
+
+    public void goToLogin() {
+        Intent intent = new Intent(SelectOptionAuthActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
