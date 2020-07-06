c72514f5f8983c7d0c0a8d8d16e5ecdd64fc64a2
MateoReinoso <dmreinoso2@espe.edu.ec>
2020-07-06 16:37:13 -0500

    no se crea el nodo

--- Desarrollo_MG/app/src/main/java/com/pm/pi_mg/RegisterActivity.java
+++ Desarrollo_MG/app/src/main/java/com/pm/pi_mg/RegisterActivity.java
@@ -1,21 +1,48 @@
 package com.pm.pi_mg;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.pm.pi_mg.models.User;
 
 public class RegisterActivity extends AppCompatActivity {
 
     Toolbar mToolbar;
     SharedPreferences mPref;
 
+    FirebaseAuth mAuth;
+    DatabaseReference mDatabase;
+
+    //instanciar vistas
+    Button mButtonRegister;
+
+    TextInputEditText mTextInputEmail;
+    TextInputEditText mTextInputName;
+    TextInputEditText mTextInputPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        //referencia al nodo principal de la base de datos creado en firebase
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         mToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
@@ -23,7 +50,82 @@ public class RegisterActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mPref = getApplicationContext().getSharedPreferences("typeUser", MODE_PRIVATE);
+
+        //Toast.makeText(this, "Opción seleccionada es: "+ selectedUser, Toast.LENGTH_SHORT).show();
+        mButtonRegister = findViewById(R.id.btnRegister);
+
+        mTextInputPassword = findViewById(R.id.textInputPassword);
+        mTextInputEmail = findViewById(R.id.textInputEmail);
+        mTextInputName = findViewById(R.id.textInputName);
+
+        mButtonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerUser();
+            }
+        });
+    }
+
+    void registerUser() {
+        final String name = mTextInputName.getText().toString();
+        final String email = mTextInputEmail.getText().toString();
+        final String password = mTextInputPassword.getText().toString();
+
+        if(!name.isEmpty() && !email.isEmpty() && !password.isEmpty()){
+            if(password.length() >= 6){
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            saveUser(name, email);
+                            Toast.makeText(RegisterActivity.this, "Registro Exitoso..", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(RegisterActivity.this, "No se pudo registrar su usuario", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+            else{
+                Toast.makeText(this, "La contraseña debe ser mayor a 6 caracteres", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else{
+            Toast.makeText(this, "Ingrese todos los campos", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    void saveUser(String name, String email) {
         String selectedUser = mPref.getString("user", "");
-        Toast.makeText(this, "Opción seleccionada es: "+ selectedUser, Toast.LENGTH_SHORT).show();
+
+        User user = new User();
+        user.setEmail(email);
+        user.setName(name);
+
+        if(selectedUser.equals("driver")){
+            //creacion de nodo hijo dentro de la base de datos
+            //push nos sirve para crear com el identificador en base de datos relacional
+            mDatabase.child("Users").child("Drivers").push().setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(RegisterActivity.this, "Registro Exitoso", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(RegisterActivity.this, "Fallo el registro", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+        else if(selectedUser.equals("client")){
+            mDatabase.child("Users").child("Clients").push().setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(RegisterActivity.this, "Registro Exitoso", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(RegisterActivity.this, "Fallo el registro", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
