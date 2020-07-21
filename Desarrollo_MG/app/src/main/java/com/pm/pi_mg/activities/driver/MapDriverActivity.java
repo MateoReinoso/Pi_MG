89991a61d01282d2d3341440d558612d7d09cce2
MateoReinoso <dmreinoso2@espe.edu.ec>
2020-07-20 20:51:28 -0500

    Driver google maps

--- Desarrollo_MG/app/src/main/java/com/pm/pi_mg/activities/driver/MapDriverActivity.java
+++ Desarrollo_MG/app/src/main/java/com/pm/pi_mg/activities/driver/MapDriverActivity.java
@@ -4,13 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.pm.pi_mg.R;
 
-public class MapDriverActivity extends AppCompatActivity {
+public class MapDriverActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_driver);
+
+        mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
     }
 }
